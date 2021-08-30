abc995818b364e4b1c404ac29d88ac0a7bd015df
James Hannah <james@jph.im>
2021-08-30 21:16:37 +0000

    client status

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,6 +23,7 @@ import (
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 
@@ -118,18 +119,27 @@ const (
 )
 
 type ClientStatus struct {
-	Status ClientStatusStatus
+	Status   ClientStatusStatus `json:"status"`
+	LastSeen time.Time          `json:"lastSeen"`
 }
 
 var (
-	imgMutex = &sync.RWMutex{}
-	clients  = map[string]ClientStatus{}
+	imgMutex    = &sync.RWMutex{}
+	clientMutex = &sync.RWMutex{}
+	clients     = map[string]ClientStatus{}
 )
 
 func (ws *WatcherServer) clientUpdate(cl mqtt.Client, msg mqtt.Message) {
 	topicparts := strings.SplitN(msg.Topic(), "/", 3)
+	log.WithFields(log.Fields{
+		"client": topicparts[2],
+		"status": string(msg.Payload()),
+	}).Info("client status update")
+	clientMutex.Lock()
+	defer clientMutex.Unlock()
 	clients[topicparts[2]] = ClientStatus{
-		Status: ClientStatusStatus(msg.Payload()),
+		Status:   ClientStatusStatus(msg.Payload()),
+		LastSeen: time.Now(),
 	}
 }
 
@@ -138,8 +148,8 @@ func (ws *WatcherServer) mqttOpts() *mqtt.ClientOptions {
 	opts.SetClientID("mqttphatserver")
 	opts.AddBroker(ws.Broker)
 	opts.OnConnect = func(client mqtt.Client) {
-		log.WithField("broker", ws.Broker).Info("mqtt broker connected")
-		client.Subscribe("phat/client/*", byte(1), ws.clientUpdate)
+		log.WithField("broker", ws.Broker).Info("MQTT broker connected")
+		client.Subscribe("phat/client/+", byte(1), ws.clientUpdate).Wait()
 	}
 	return opts
 }
@@ -161,52 +171,64 @@ func (ws *WatcherServer) publishPost() error {
 	}
 }
 
-func (ws *WatcherServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	defer func(req *http.Request) {
-		log.WithFields(log.Fields{
-			"client-addr": req.RemoteAddr,
-			"method":      req.Method,
-		}).Infof("HTTP %s", req.URL.Path)
-	}(req)
+func (ws *WatcherServer) ImageDownload(rw http.ResponseWriter, req *http.Request) {
+	imgMutex.RLock()
+	defer imgMutex.RUnlock()
+	if ws.lastimg == nil {
+		log.Warn("GET request but no image ready for download")
+		rw.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(rw, "No image cached yet\n")
+		return
+	} else {
+		log.WithField("format", ws.lastimg.Type).Info("GET request, serving cached image")
+		rw.Header().Add("Content-Type", ws.lastimg.Type)
+		rw.Header().Set("ETag", ws.lastimg.ETag())
+		// wb, err := rw.Write(ws.lastimg.image)
+		http.ServeContent(rw, req, "", ws.lastimg.StoredAt, bytes.NewReader(ws.lastimg.Image))
+	}
+}
+
+func (ws *WatcherServer) ListClients(rw http.ResponseWriter, req *http.Request) {
+	type clientsListOutput struct {
+		Clients map[string]ClientStatus `json:"clients"`
+	}
+	clientMutex.RLock()
+	defer clientMutex.RUnlock()
+	output := clientsListOutput{
+		Clients: clients,
+	}
+	enc := json.NewEncoder(rw)
+	err := enc.Encode(output)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "unable to encode JSON: %s", err)
+	}
+}
+
+func (ws *WatcherServer) ImageUpload(rw http.ResponseWriter, req *http.Request) {
 	var err error
-	switch req.Method {
-	case http.MethodPut:
-		if strings.HasPrefix(req.Header.Get("Content-Type"), "image/") {
-			imgMutex.Lock()
-			defer imgMutex.Unlock()
-			imageIsRawVal := req.URL.Query().Get("raw")
-			ws.lastimg, err = CreateImage(req.Body, imageIsRawVal != "true")
-			if err != nil {
-				rw.WriteHeader(http.StatusNotAcceptable)
-				fmt.Fprintf(rw, "ERROR: %s", err.Error())
-				return
-			}
-			rw.WriteHeader(http.StatusCreated)
-			log.WithFields(log.Fields{
-				"size":   len(ws.lastimg.Image),
-				"format": ws.lastimg.Type,
-			}).Info("stored image")
-			// Actually push the thing to MQTT
-			go ws.publishPost()
-			return
-		} else {
+	if strings.HasPrefix(req.Header.Get("Content-Type"), "image/") {
+		imgMutex.Lock()
+		defer imgMutex.Unlock()
+		imageIsRawVal := req.URL.Query().Get("raw")
+		ws.lastimg, err = CreateImage(req.Body, imageIsRawVal != "true")
+		if err != nil {
 			rw.WriteHeader(http.StatusNotAcceptable)
-			fmt.Fprintf(rw, "ERROR: Content-type not image/*")
+			fmt.Fprintf(rw, "ERROR: %s", err.Error())
 			return
 		}
-	case http.MethodGet:
-		imgMutex.RLock()
-		defer imgMutex.RUnlock()
-		if ws.lastimg == nil {
-			rw.WriteHeader(http.StatusNotFound)
-			return
-		} else {
-			log.WithField("format", ws.lastimg.Type).Info("GET request, serving cached image")
-			rw.Header().Add("Content-Type", ws.lastimg.Type)
-			rw.Header().Set("ETag", ws.lastimg.ETag())
-			// wb, err := rw.Write(ws.lastimg.image)
-			http.ServeContent(rw, req, "", ws.lastimg.StoredAt, bytes.NewReader(ws.lastimg.Image))
-		}
+		rw.WriteHeader(http.StatusCreated)
+		log.WithFields(log.Fields{
+			"size":   len(ws.lastimg.Image),
+			"format": ws.lastimg.Type,
+		}).Info("stored image")
+		// Actually push the thing to MQTT
+		go ws.publishPost()
+		return
+	} else {
+		rw.WriteHeader(http.StatusNotAcceptable)
+		fmt.Fprintf(rw, "ERROR: Content-type not image/*")
+		return
 	}
 }
 
@@ -218,13 +240,53 @@ func (ws *WatcherServer) runPeriodicPublisher(every time.Duration) {
 	}
 }
 
+type StatsWriter struct {
+	http.ResponseWriter
+	BytesWritten int
+	WroteStatus  int
+}
+
+func (sw *StatsWriter) WriteHeader(statusCode int) {
+	sw.WroteStatus = statusCode
+	sw.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (sw *StatsWriter) Write(p []byte) (n int, err error) {
+	n, err = sw.ResponseWriter.Write(p)
+	sw.BytesWritten += n
+	return n, err
+}
+
+func logMw(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		sw := &StatsWriter{
+			ResponseWriter: w,
+		}
+		tStart := time.Now()
+		next.ServeHTTP(sw, req)
+		log.WithFields(log.Fields{
+			"client-addr":   req.RemoteAddr,
+			"response-size": sw.BytesWritten,
+			"code":          sw.WroteStatus,
+			"duration":      time.Since(tStart),
+		}).Infof("%s %s", req.Method, req.URL.Path)
+	})
+}
+
 func (ws *WatcherServer) Run(ctx context.Context) error {
 	ws.client = mqtt.NewClient(ws.mqttOpts())
 	if token := ws.client.Connect(); token.Wait() && token.Error() != nil {
 		return token.Error()
 	}
 	go ws.runPeriodicPublisher(10 * 60 * time.Second)
-	return http.ListenAndServe(ws.ListenAddr, ws)
+	r := mux.NewRouter()
+	r.Use(logMw)
+	r.Methods(http.MethodPut).HandlerFunc(ws.ImageUpload)
+	getroutes := r.Methods(http.MethodGet).Subrouter()
+	getroutes.Path("/clients").HandlerFunc(ws.ListClients)
+	getroutes.Path("/").HandlerFunc(ws.ImageDownload)
+	log.WithField("listen-addr", ws.ListenAddr).Info("starting HTTP server")
+	return http.ListenAndServe(ws.ListenAddr, r)
 }
 
 func main() {
