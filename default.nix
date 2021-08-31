0c2981ad9f474c786bab55b18f9419448865e7c6
James Hannah <james.hannah@stfc.ac.uk>
2021-08-31 10:53:57 +0100

    nix

--- default.nix
+++ default.nix
@@ -2,15 +2,15 @@ with import <nixpkgs> {};
 #{ stdenv, buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   name = "phatmqttserver";
-  version = "0.0.3";
+  version = "0.0.10";
   src = fetchFromGitHub {
     owner = "jiphex";
     repo = name;
     rev = "v${version}";
-    sha256 = "0ginmy1msf4h3p0qr8r9fqqssk2vkrcllg3csp96mbcw126y6pn1";
+    sha256 = "0wdn1bblr2halfklab5acj9z7aah79fkwswah0rwiv5lfwp1lg5s";
   };
-  vendorSha256 = "03hz53j02xalaphm0k5dz226v9cjrkzq1n54x48sqnlmfc7ccyys";
-  subPackages = [ "cmd/imgtool" "cmd/server" ];
+  vendorSha256 = "0sa0imkm6sgq4bc1k3zml188q4z7smjphjdrkbrspdpf3i2wqyc3";
+  subPackages = [ "cmd/server" ];
   runVend = false;
   meta = with stdenv.lib; {};
 }
